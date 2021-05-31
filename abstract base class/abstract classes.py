6a70fd9e582276432520bb30c61672151c3e9a79
Prithivirajan Karthikeyan <prithivirajanka@gmail.com>
2021-05-31 15:18:03 -0400

    added abstract classes

---
+++ abstract base class/abstract classes.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self): pass
+
+    @abstractmethod
+    def perimeter(self): pass
+
+class Square(Shape):
+    def __init__(self, side):
+        self.__side = side
+
+    def area(self):
+        return self.__side * self.__side
+
+    def perimeter(self):
+        return self.__side * 4
+
+sq = Square(5)
+print(sq.perimeter())
